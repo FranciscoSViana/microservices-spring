bfc792d0dba8c9819df30d712b0ec42e9a1a60f5
Francisco Viana <franciscosantos29@gmail.com>
2022-06-10 21:48:38 -0300

    Implementando OAuth2

--- loja/src/main/java/br/com/fsv/microservice/loja/LojaApplication.java
+++ loja/src/main/java/br/com/fsv/microservice/loja/LojaApplication.java
@@ -6,13 +6,42 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.web.client.RestTemplate;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 
 @SuppressWarnings("deprecation")
 @SpringBootApplication
 @EnableFeignClients
 @EnableCircuitBreaker
+@EnableScheduling
+@EnableResourceServer
 public class LojaApplication {
+	
+	@Bean
+	public RequestInterceptor getInterceptonDeAutenticacao() {
+		
+		return new RequestInterceptor() {
+			
+			@Override
+			public void apply(RequestTemplate template) {
+				Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+				
+				if (authentication == null) {
+					return;
+				}
+				
+				OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) authentication.getDetails();
+				template.header("Authorization", "Beater" + details.getTokenValue());
+			}
+		};
+	}
 	
 	@Bean
 	@LoadBalanced
